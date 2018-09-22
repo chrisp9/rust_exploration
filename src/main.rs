f0a484d21b5ef33d5971d0e60bf5443c20daf12f
chrisp9 <chris_powell9@outlook.com>
2018-09-22 19:36:04 +0100

    Wrapping up structs

--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,38 @@ struct User {
     active: bool
 }
 
+struct Rectangle {
+    width: u32,
+    height: u32
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle {width: size, height: size}
+    }
+}
+
+
 fn main() {
+    let rect1 = Rectangle {width: 30, height: 50};
+    let rect2 = Rectangle {width: 10, height: 40};
+    let rect3 = Rectangle {width: 60, height: 45};
 
-    let user1 = build_user(String::from("chris@lol.com"), String::from("abcd"));
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect2 hold rect1? {}", rect2.can_hold(&rect1));
+}
 
-
+fn rectangles() {
+    let rect1 = Rectangle { width: 30, height: 50};
+    println!("The area of the rectangle is {} square pixels.", rect1.area());
 
 }
 
@@ -27,6 +54,7 @@ fn build_user(email: String, username: String) -> User {
         sign_in_count: 1
     }
 }
+
 
 
 fn slices() {
